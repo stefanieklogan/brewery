9201090425d3ef4e6bb3b1bf19e8c91275babbd8
rsco <RaquelLee@users.noreply.github.com>
2021-05-09 18:08:04 -0400

    home page photo displaying

--- src/components/HomePagePic/index.js
+++ src/components/HomePagePic/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Image from 'react-bootstrap/Image';
-
+import Logo from "../../assets/Celebration Brewing.png"
 function HomePagePic() {
     return (
         <Container>
             <Row>
-                <Image src="../../assets/Celebration Brewing.png" />
+                <Image src={Logo} />
             </Row>
         </Container>
     )
