3011a6ca050c9c16f52033b3877d156680431016
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-05-09 16:18:18 -0400

    updated links

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -2,13 +2,23 @@ import React from "react";
 import "./style.css";
 import { makeStyles } from "@material-ui/core/styles";
 import InstagramIcon from "@material-ui/icons/Instagram";
-import GitHubIcon from "@material-ui/icons/GitHub";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import TwitterIcon from "@material-ui/icons/Twitter";
+import { Icon, InlineIcon } from '@iconify/react';
+import untappdIcon from '@iconify-icons/simple-icons/untappd';
 
 
 const useStyles = makeStyles({
     icons: {
+        marginRight: "25px",
+        marginLeft: "25px",
+        color: "black",
+        '&:hover': {
+            color: "#c89019",
+        }   
+    },
+    beerIcon: {
+       fontSize: "30px",
         marginRight: "25px",
         marginLeft: "25px",
         color: "black",
@@ -29,10 +39,10 @@ function Jumbotron() {
         <h1 className="display-4">Celebration Brewery Logo</h1>
     </div>
     <div className="center">
-        <a href="https://github.com/marisanesmith" rel="noreferrer" target="_blank" className={styles.icons}><GitHubIcon fontSize="large"/></a>
-        <a href="https://www.instagram.com/thisworldtraveled" rel="noreferrer" target="_blank" className={styles.icons}><InstagramIcon fontSize="large"/></a>
-        <a href="https://www.linkedin.com/in/marisa-nesmith" rel="noreferrer" target="_blank" className={styles.icons}><FacebookIcon fontSize="large"/></a>
-        <a href="https://www.thisworldtraveled.com" rel="noreferrer" target="_blank" className={styles.icons}><TwitterIcon fontSize="large"/></a>
+        <a href="https://untappd.com/CelebrationBrewing" rel="noreferrer" target="_blank" className={styles.beerIcon}><Icon icon={untappdIcon} /></a>
+        <a href="https://www.instagram.com/celebrationbrewing/" rel="noreferrer" target="_blank" className={styles.icons}><InstagramIcon fontSize="large"/></a>
+        <a href="https://www.facebook.com/CelebrationBrewing" rel="noreferrer" target="_blank" className={styles.icons}><FacebookIcon fontSize="large"/></a>
+        <a href="https://twitter.com/CelebrationBrew" rel="noreferrer" target="_blank" className={styles.icons}><TwitterIcon fontSize="large"/></a>
     </div>
     </body>
 
