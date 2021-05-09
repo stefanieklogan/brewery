d172857a1cd377255201e4086fce68efdc2d8af5
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-05-09 18:02:30 -0400

    added more components and forms

--- src/components/HomePageAbout/index.js
+++ src/components/HomePageAbout/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
+import Button from '@material-ui/core/button';
 
 
 const useStyles = makeStyles({
@@ -17,7 +18,7 @@ function HomePageAbout() {
             <body>
             <h2 className={styles.titleStyle}> ORLANDO'S FAVORITE BREWERY</h2>
             <p>A blurb about Celebration Brewery. How it started, where it's located, etc. Short and sweet description</p>
-            
+            <Button variant="contained"> Visit Us </Button>
     </body>
 </div>
         
