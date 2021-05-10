3e74f62689ed26a87729bf931729770de22ac6fd
rsco <RaquelLee@users.noreply.github.com>
2021-05-10 12:26:51 -0400

    untappd passport strategy initialized

--- routes/api/untappdRoutes.js
+++ routes/api/untappdRoutes.js
@@ -1,9 +1,31 @@
 const router = require("express").Router();
+const passport = require("passport");
+
+passport.use(new UntappdStrategy({
+	clientID: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET,
+	// redirect after login
+	callbackURL: 'https://www.example.net/auth/untappd/callback'
+}, function (accessToken, refreshToken, profile, done) {
+	User.findOrCreate({ untappdId: profile.id }, function (err, user) {
+		done(err, user);
+	});
+}));
+
+app.get('/auth/untappd', passport.authenticate('untappd'));
+
+app.get('/auth/untappd/callback',
+	passport.authenticate('untappd', { failureRedirect: '/login' }),
+	function (req, res) {
+		// Successful authentication, redirect home.
+		res.redirect('/');
+	});
+	
 var UntappdClient = require("node-untappd");
 var debug = false;
 var untappd = new UntappdClient(debug);
 
-var clientId = process.env.CLIENT_ID ;
+var clientId = process.env.CLIENT_ID;
 var clientSecret = process.env.CLIENT_SECRET;
 
 // Set to true if you want to see all sort of nasty output on stdout
@@ -11,7 +33,7 @@ var debug = false;
 
 // The user we want to lookup for this example
 var data = {};
-	data.USERNAME = "[ some user name ]";
+data.USERNAME = "[ some user name ]";
 
 // Create Client
 var untappd = new UntappdClient(debug);
@@ -19,10 +41,10 @@ untappd.setClientId(clientId);
 untappd.setClientSecret(clientSecret);
 
 // EXAMPLE - List last 25 recent checkins of the given user
-untappd.userActivityFeed(function(err,obj){
-	if (debug) console.log(err,obj);
+untappd.userActivityFeed(function (err, obj) {
+	if (debug) console.log(err, obj);
 	if (obj && obj.response && obj.response.checkins && obj.response.checkins.items) {
-		var beers = obj.response.checkins.items.forEach(function(checkin) {
+		var beers = obj.response.checkins.items.forEach(function (checkin) {
 			console.log(checkin);
 			console.log(checkin.user.user_name, "drank", checkin.beer.beer_name);
 			console.log("by", checkin.brewery.brewery_name);
