137660d873502d61458984b60e6f4f7c319d133e
marisanesmith <marisa@thisworldtraveled.com>
2021-05-14 08:24:07 -0400

    updated beer cards

--- client/src/pages/BeerPage/index.js
+++ client/src/pages/BeerPage/index.js
@@ -50,17 +50,6 @@ export default function MediaControlCard() {
             This will be a paragraph form of a beer description
           </Typography>
         </div>
-        {/* <div className={classes.controls}>
-          <IconButton aria-label="previous">
-            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-          </IconButton>
-          <IconButton aria-label="play/pause">
-            <PlayArrowIcon className={classes.playIcon} />
-          </IconButton>
-          <IconButton aria-label="next">
-            {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-          </IconButton>
-        </div> */}
       </div>
       <CardMedia
         className={classes.cover}
