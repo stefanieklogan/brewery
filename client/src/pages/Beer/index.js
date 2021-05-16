aeaeb41d888db4cdf0207a93b9bbe99d02922ab9
rsco <RaquelLee@users.noreply.github.com>
2021-05-16 14:53:02 -0400

    component state reflects beer list, beer list presented

--- client/src/pages/Beer/index.js
+++ client/src/pages/Beer/index.js
@@ -1,17 +1,38 @@
 import React, { useState, useEffect } from "react";
-import BeerCard from '../../components/BeerCard';
 import API from "../../utils/API.js";
+//     CardActions removed from line 5 after typography
+import { makeStyles} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    content: {
+        flex: '1 0 auto',
+    },
+    cover: {
+        width: 100,
+    }
+}));
 
 function Beer() {
-    // Setting our component's initial state
+    const classes = useStyles();
+
     const [beers, setBeers] = useState([])
 
-    // Load all beers and store them with setBeers
     useEffect(() => {
         loadBeers()
     }, [])
 
-    // Loads all beers and sets them to beers
     function loadBeers() {
         API.getBeers()
             .then(res =>
@@ -19,27 +40,35 @@ function Beer() {
             )
             .catch(err => console.log(err));
     };
-
     return (
-            <main>
-                <div className="mb-5"><h2>Beers on Tap</h2></div>
-                <div className="container-fluid p-4">
-                    <div className="col">
-                        {beers.map((beer) => (
-                            <div className="card-body">
-                                <div className="col-6">
-                                    <BeerCard>
-                                        {beer.items}
-                                    </BeerCard>
-                                </div>
+        <main>
+            <div className="mb-5">
+                <h2>Beers on Tap</h2></div>
+            <Grid container spacing={3}>
+                {beers.map((beer) => (
+                    <Grid key={beer.beer.beer_name} item xs={6}>
+                        <Card className={classes.root}>
+                            <div className={classes.details}>
+                                <CardContent className={classes.content}>
+                                    <Typography component="h5" variant="h5">
+                                        {beer.beer.beer_name}
+                                    </Typography>
+                                    <Typography variant="subtitle1" color="textSecondary">
+                                        {beer.beer.beer_description}
+                                    </Typography>
+                                </CardContent>
                             </div>
-                        ))}
-                    </div>
-                </div>
-            </main>
-
-        )
-    }
-
+                            <CardMedia
+                                className={classes.cover}
+                                image={beer.beer.beer_label}
+                                alt={beer.beer.beer_name}
+                            />
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    )
+}
 
 export default Beer;
