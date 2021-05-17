b6991f0c463113d1ed8bfd84439fed475f34c805
rsco <RaquelLee@users.noreply.github.com>
2021-05-16 20:49:08 -0400

    get route added to frontend api routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,8 @@ import axios from "axios";
 export default {
   getBeers: function() {
     return axios.get("/api/untappd");
+  },
+  getFeedback: function() {
+    return axios.get("/api/feedback");
   }
 };
