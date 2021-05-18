668ce0545b6f51080d32fcbcbf99fae254fbc6a6
rsco <RaquelLee@users.noreply.github.com>
2021-05-18 15:12:53 -0400

    api routes added for react

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
 export default {
-  getBeers: function () {
+  // postLogin: function() {
+  //   return axios.post("/api/user/login");
+  // },
+  // postSignup: function() {
+  //   return axios.post("/api/signup");
+  // },
+  getBeers: function() {
     return axios.get("/api/untappd");
   },
-  getFeedback: function () {
+  getFeedback: function() {
     return axios.get("/api/feedback");
   }
 };
