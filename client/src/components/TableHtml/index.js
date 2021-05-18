7fc5eb83ad174dcdeb28f782c1d1e21ff72a3256
rsco <RaquelLee@users.noreply.github.com>
2021-05-17 20:53:36 -0400

    feedback displaying from table html

--- client/src/components/TableHtml/index.js
+++ client/src/components/TableHtml/index.js
@@ -1,32 +1,46 @@
 import React from 'react';
 import "./style.css";
 
-function TableHtml (props)  {
-    // let { format, headings, rows, click } = props;
-  let { format, headings, row, id } = props;
+function TableHtml({ format, headings, rows }) {
+  // let { format, headings, rows, click } = props;
+  console.log("tablehtml, line 6", rows)
   return (
-        <table className={`table ${format || "table-striped"}`}>
-        <thead>
-          <tr>
-            {headings.map(heading => {if(heading==="Name") {
-            // return <th scope="col" onClick={click}>{heading}</th>
-            return <th key={heading} scope="col">{heading}</th>
+    <table className={`table ${format || "table-striped"}`}>
+      <thead>
+        <tr>
+          {headings.map(heading => {
+            if (heading === "Name") {
+              // return <th scope="col" onClick={click}>{heading}</th>
+              return <th key={heading} scope="col">{heading}</th>
             }
-            else {return <th key={heading} scope="col">{heading}</th>}})
+            else { return <th key={heading} scope="col">{heading}</th> }
+          })
           }
-          </tr>
-        </thead>
-        <tbody>      
-          {row.map((item) => (
-            <tr key={item.name}>
-              <td key={id}>
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((item) => {
+          return <tr key={item.id}>
+            <td>
+              {item.date_created}
+            </td>
+            <td>
               {item.name}
-              </td>
-              </tr>
-          )
-          )}
-        </tbody>
-      </table>
+            </td>
+            <td>
+              {item.email}
+            </td>
+            <td>
+              {item.checkbox ? "Yes" : "No"}
+            </td>
+            <td>
+              {item.feedback}
+            </td>
+          </tr>
+        }
+        )}
+      </tbody>
+    </table>
   );
 }
 
