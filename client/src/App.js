6e76fe79f246122e88f67a909740b3b0e15a8419
rsco <RaquelLee@users.noreply.github.com>
2021-05-18 09:28:38 -0400

    react hashrouter replaced with browserrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,41 @@
 import React from 'react';
-//, { Component } removed after React (no unused vars)
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from './components/NavBar';
-import Footer from './components/Footer';
 // import Copyright from './components/Copyright';
-import Jumbotron from './components/Jumbotron';
-import { HashRouter, Route, Switch } from 'react-router-dom';
-import Image from './components/Image';
-import HomePageAbout from './components/HomePageAbout';
-// import SignUpForm from './components/SignUpForm';
-import FeedbackForm from './components/FeedbackForm';
-import AdminLogin from './components/AdminLogin';
+import Homepage from './pages/Home'
 import Beer from './pages/Beer';
 import AboutUs from './pages/AboutUs';
+import Feedback from './pages/FeedbackForm';
 import VisitUs from './pages/VisitUs';
-import FeedbackResults from './components/FeedbackResults';
-
-function Homepage() {
-
-  return (
-    <div>
-        <Jumbotron />
-        <Image />
-        <HomePageAbout />
-        {/* <SignUpForm /> */}
-        <Footer />
-        {/* <Copyright /> */}
-    </div>
-  );
-
-}
+import SignUp from "./pages/Signup"
+import Login from './pages/AdminLogin';
+import FeedbackResults from './pages/FeedbackResults';
 
 function App() {
-
   return (
-    <div className="App">
-      <HashRouter basename="/">
+      <Router>
         <NavBar />
         <Switch>
-          <Route exact path="/feedback"><FeedbackForm /></Route>
-          <Route exact path="/admin"><AdminLogin /></Route>
-          <Route exact path="/"><Homepage /></Route>
-          <Route exact path="/beer"><Beer /></Route>
-          <Route exact path="/about"><AboutUs /></Route>
-          <Route exact path="/visit"><VisitUs /></Route>
-          <Route exact path='/admin/feedbackresults'><FeedbackResults /></Route>
+        <Route exact path="/">
+          <Homepage /></Route>
+        <Route exact path="/beer">
+          <Beer /></Route>
+        <Route exact path="/about">
+          <AboutUs /></Route>
+          <Route exact path="/contact">
+            <Feedback /></Route>
+          <Route exact path="/visit">
+            <VisitUs /></Route>
+          <Route exact path="/signup">
+            <SignUp /></Route>
+          <Route exact path="/admin">
+            <Login /></Route>
+          <Route exact path='/admin/feedbackresults'>
+            <FeedbackResults /></Route>
           </Switch>
           {/* <Copyright /> */}
-      </HashRouter>
-    </div>
+          </Router>
   );
-
 }
 
 export default App;
