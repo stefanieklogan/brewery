5b9c111943f580415009512c645061f9d3b978ae
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-05-21 13:30:55 -0400

    updated contact page

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,89 +1,111 @@
-import React from 'react';
-import Footer from '../components/Footer';
-import Copyright from '../components/Copyright';
-import { Grid } from '@material-ui/core';
+import React from "react";
+import Footer from "../components/Footer";
+import Copyright from "../components/Copyright";
+import { Grid, Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 // import Logo from "../assets/Celebration_Logo.png";
 import BeerPhoto from "../assets/about-celebration.png";
 // import BeerGlass from "../assets/beer-glass";
 // import beerImage from "../../assets/brewery.jpg";
-import Row from 'react-bootstrap/Row';
+import Row from "react-bootstrap/Row";
 // import Col from 'react-bootstrap/Col';
 // import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
+import Image from "react-bootstrap/Image";
 // import { ImageList } from '@material-ui/core';
 // import ImageListItem from '@material-ui/core/ImageListItem';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            textAlign: "center",
-        },
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      textAlign: "center",
     },
-    titleStyle: {
-        // backgroundColor: "white",
-        color: "#c89019",
-        textAlign: "center",
-        // postition: "absolute",
-        marginTop: "70px",
-        bottom: "80px",
-        right: "50px",
-    },
-    para: {
-        textAlign: "center",
-        marginTop: "30px",
-        paddingRight: "100px",
-        paddingLeft: "100px",
-    },
-    button: {
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    image: {
-        height: "100%",
-        width: "100%",
-        // position: "relative",
-        // paddingTop: "50px",
-    },
-    beerImage: {
-        height: "350px",
-
-    }
+  },
+  titleStyle: {
+    // backgroundColor: "white",
+    color: "#c89019",
+    textAlign: "center",
+    // postition: "absolute",
+    marginTop: "70px",
+    bottom: "80px",
+    right: "50px",
+  },
+  para: {
+    textAlign: "center",
+    marginTop: "30px",
+    paddingRight: "100px",
+    paddingLeft: "100px",
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+    // position: "relative",
+    // paddingTop: "50px",
+  },
+  beerImage: {
+    height: "350px",
+  },
 }));
 
 function AboutUs() {
-    const styles = useStyles();
-    return (
-        <div>
-            <Grid>                 
-                <div className={styles.image}>
-                    <Image src={BeerPhoto} alt="Celebration Brewing Logo" fluid />
-                </div>
-            {/* <Typography className={styles.titleStyle} component="h3" variant="h3">
+const styles = useStyles();
+return (
+  <div>
+    <Grid>
+      <div className={styles.image}>
+        <Image src={BeerPhoto} alt="Celebration Brewing Logo" fluid />
+      </div>
+      {/* <Typography className={styles.titleStyle} component="h3" variant="h3">
                 About Celebration Brewing
             </Typography> */}
-            
-            <Typography className={styles.para} component="p" variant="inherit">
-                Since 2021, Celebration Brewing Company has been committed to producing authentic and unique beers that push the boundaries of regular brewing styles and categories. We’re proud to serve our customers with only the best, continually crafting each taste until it’s exactly right.
-            <br />
-                <br />
-            Celebration Brewing is a family-owned brewery, taproom, and kitchen located in beautiful Celebration, Florida. We focus on using only the best ingredients and delivering the utmost quality to our customers.
-            <br />
-                <br />
-            Everyday, we’re driven by our core values: delivering quality flavors, honoring our customers and celebrating the culture of beer. Experience it for yourself by joining us for a brewery tour, a special event or just to try what’s on tap.
-            </Typography>
-            <Grid container direction="row" spacing={1} style={{ width: "100%", alignItems: "center", marginTop: "40px"}} justify="center">
-                <Button variant="outlined" href="#"> Visit Us </Button>
-            </Grid>
-            <Footer />
-            <Copyright />
-            </Grid>
-        </div>
-    )
-};
 
-export default AboutUs
+      <Typography className={styles.para} component="p" variant="inherit">
+        Frank L began dreaming of his vision for Celebration Brewing while
+        homebrewing right here in Main Village, Celebration. Brewing a keg here,
+        pouring a pint there, it’s good to be friends with Frank! As a resident
+        for over 15 years, Frank saw the need for creating community for those
+        who enjoy good beer and good company. Fast forward to today where he’s
+        turning his dream into a reality.
+        <br />
+        <br />
+        At Celebration Brewing we’re here to serve our community one pint at a
+        time. Our beers are crafted to bring our community together. Whether you
+        call Celebration home or just come to visit - stop by and taste what’s
+        on tap.
+        <br />
+        <br />
+        Life’s short. Love what you do, do what you love. #CelebrationBrewing
+    </Typography>
+    <Grid
+        container
+        direction="row"
+        spacing={1}
+        style={{ width: "100%", alignItems: "center", marginTop: "40px" }}
+        justify="center"
+    >
+        <Button variant="outlined" href="#">
+            Visit Us{" "}
+        </Button>
+    </Grid>
+    <Container style={{ marginTop: "3%", textAlign: "center", padding: "2%", border: "solid 1px", borderRadius: "8px"}}>
+        <Typography style={{marginTop: "1%", marginBottom: "2%"}} component="h4" variant="h4">
+            BREAKING GROUND <span style={{color: "#c89019"}}> | </span> SUMMER 2021
+        </Typography>
+        <Typography component="h4" variant="h4">
+            OPENING <span style={{color: "#c89019"}}> | </span> WINTER 2022
+        </Typography>
+    </Container>
+    <Footer />
+    <Copyright />
+    </Grid>
+</div>
+);
+}
 
+export default AboutUs;
--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Container, Grid } from '@material-ui/core';
+import { Typography, Container, Grid, Zoom } from '@material-ui/core';
 // import { sizing } from '@material-ui/system';
 import FeedbackForm from '../components/FeedbackForm/';
 import Footer from '../components/Footer/';
@@ -7,6 +7,7 @@ import Copyright from '../components/Copyright/';
 import Image from 'react-bootstrap/Image';
 import Beer from "../assets/contact-celebration.png";
 import { makeStyles } from "@material-ui/core/styles";
+import { Transition } from 'react-transition-group';
 
 
 const useStyles = makeStyles({
@@ -47,10 +48,46 @@ const useStyles = makeStyles({
         // flexWrap: "wrap",
         // marginTop: "50px",
     }
-})
+});
 
 function Contact() {
     const styles = useStyles();
+
+    // const duration = 300;
+
+    // const defaultStyle = {
+    //     transition: `opacity ${duration}ms ease-in-out`,
+    //     opacity: 0,
+    // }
+
+    // const transitionStyles= {
+    //     entering: { opacity: 1 },
+    //     entered: { opacity: 1 },
+    //     exiting: { opacity: 0 },
+    //     exited: { opacity: 0 },
+    // };
+
+    // const Fade = ({ in: inProp }) => (
+    //     <Transition in={inProp} timeout={duration}>
+    //         {state => (
+    //             <div style={{
+    //                 ...defaultStyle,
+    //                 ...transitionStyles[state]
+    //             }}>
+    //                 Breaking Ground
+    //             </div>
+    //         )}
+    //     </Transition>
+    // // );
+
+    // const documentOptions = {
+    //     loop: true,
+    //     autoplay: true,
+    //     animationData: documentsAnimation,
+    //     rendererSettings: {
+    //         presereAspectRatio: "xMidYMid slice"
+    //     }
+    // };
 
     return (
         <Grid>
@@ -61,12 +98,23 @@ function Contact() {
             {/* <Typography className={styles.title} component="h3" variant="h3">
                 CONTACT US
                 </Typography> */}
+                {/* <Zoom { ...documentOptions}>   */}
+                <Container style={{ marginTop: "3%", textAlign: "center", padding: "2%", border: "solid 1px", borderRadius: "8px"}}>
+                    <Typography style={{marginTop: "1%", marginBottom: "2%"}} component="h4" variant="h4">
+                        BREAKING GROUND <span style={{color: "#c89019"}}> | </span> SUMMER 2021
+                    </Typography>
+                    <Typography component="h4" variant="h4">
+                        OPENING <span style={{color: "#c89019"}}> | </span> WINTER 2022
+                    </Typography>
+                </Container>
+                
+                {/* </Zoom > */}
                 <Typography className={styles.para} component="p" variant="h6">
                 Located in the Celebration area of Orlando, Celebration Brewing is the best brewery in town. We offer brewery tours so that guests can learn about how we produce out beers,etc.
                 </Typography>
             <Container style={{backgroundColor:"black", padding: "2em"}}>
             <Typography className={styles.h4} component="h4" variant="h4">
-                Get in Touch
+                Share Feedback
                 </Typography>
                 <Typography className={styles.paraTwo} component="p" variant="inherit"> We value your input and feedback! Please fill out the form below and we will get in touch as soon as possible! Thank you!
                 </Typography>
