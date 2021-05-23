8916e5368028c975a3083001882d683e0cd29bdd
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-05-23 19:35:25 -0400

    debug chimp

--- client/src/components/Mailchimp/index.js
+++ client/src/components/Mailchimp/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import MailchimpSubscribe from "react-mailchimp-subscribe";
+// import React from 'react';
+// import MailchimpSubscribe from "react-mailchimp-subscribe";
 
-const url = process.env.REACT_APP_MAILCHIMP_URL;
+// const url = process.env.REACT_APP_MAILCHIMP_URL;
 
-const SimpleForm = () => <MailchimpSubscribe url={url} />
+// const SimpleForm = () => <MailchimpSubscribe url={url} />
 
-const MailchimpForm = () => (
-    <MailchimpSubscribe
-    url={url}
-    render={({ subscribe, status, message }) => (
-        <div>
-            <SimpleForm onSubmitted={formData => subscribe(formData)} />
-            {status === "sending" && <div style={{ color: "blue"}}>sending...</div>}
-            {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
-            {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
-        </div>
-    )}
-    />
-)
+// const MailchimpForm = () => (
+//     <MailchimpSubscribe
+//     url={url}
+//     render={({ subscribe, status, message }) => (
+//         <div>
+//             <SimpleForm onSubmitted={formData => subscribe(formData)} />
+//             {status === "sending" && <div style={{ color: "blue"}}>sending...</div>}
+//             {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
+//             {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
+//         </div>
+//     )}
+//     />
+// )
 
-export default MailchimpForm
+// export default MailchimpForm
+
+
 
 
 
